93e0b407a1c57df3aa1537d06f1227fe005cec2c
Diego Armange Costa <diego.armange.costa@gmail.com>
2019-04-03 10:07:58 -0300

    New project ftc renaming city to snb.

---
+++ backend-api-examples-ftc-api/src/main/java/br/com/armange/backend/api/ftc/jaxrs/application/Application.java
@@ -0,0 +1,27 @@
+package br.com.armange.backend.api.ftc.jaxrs.application;
+
+import javax.ws.rs.ApplicationPath;
+
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.ServerProperties;
+
+import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
+
+import br.com.armange.backend.api.ftc.rest.resource.CityResource;
+
+@ApplicationPath("/")
+public class Application extends ResourceConfig {
+
+    public Application() {
+        loadResources();
+        
+        property(ServerProperties.BV_SEND_ERROR_IN_RESPONSE, true);
+    }
+    
+    private void loadResources() {
+        register(CityResource.class);
+        register(MultiPartFeature.class);
+        register(JacksonJaxbJsonProvider.class);
+    }
+}
---
+++ backend-api-examples-ftc-api/src/main/java/br/com/armange/backend/api/ftc/rest/resource/CityResource.java
@@ -0,0 +1,49 @@
+package br.com.armange.backend.api.ftc.rest.resource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+
+import br.com.armange.codeless.objectbuilder.ObjectBuilder;
+import br.com.armange.codeless.objectbuilder.csv.CsvLine;
+
+@Path("/city")
+public class CityResource {
+
+    @POST
+    @Path("/csv")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response uploadFile(
+        @FormDataParam("file") final InputStream uploadedInputStream,
+        @FormDataParam("file") final FormDataContentDisposition fileDetail,
+        @QueryParam("separator") final String separator,
+        @QueryParam("hasHeader") final String hasHeader) {
+
+        try {
+            final Set<CsvLine> set = ObjectBuilder
+                .ofInputStream(uploadedInputStream)
+                .ofCsv(separator, new Boolean(hasHeader))
+                .stream()
+                .collect(Collectors.toSet());
+            
+            return Response.status(200).entity(set).build();
+        } catch (final IOException e) {
+            e.printStackTrace();
+            return Response.status(Status.BAD_REQUEST).build();
+        }
+    }
+}
