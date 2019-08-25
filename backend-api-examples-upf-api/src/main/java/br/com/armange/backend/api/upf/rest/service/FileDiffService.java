d84c317e23d628ecdfd72adbca1b8f778ae8e740
Diego Armange Costa <diego.armange.costa@gmail.com>
2019-08-25 20:12:35 -0300

    New file-comparator.

--- backend-api-examples-upf-api/src/main/java/br/com/armange/backend/api/upf/rest/service/FileDiffService.java
+++ backend-api-examples-upf-api/src/main/java/br/com/armange/backend/api/upf/rest/service/FileDiffService.java
@@ -1,11 +1,15 @@
 package br.com.armange.backend.api.upf.rest.service;
 
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.apache.commons.lang3.StringUtils;
+
+import com.github.difflib.algorithm.DiffException;
+import com.github.difflib.text.DiffRow;
+import com.github.difflib.text.DiffRowGenerator;
 
 public class FileDiffService {
     private static final String DELETED_LINE_PREFIX = "- ";
@@ -22,81 +26,74 @@ public class FileDiffService {
         return new FileDiffService(id);
     }
     
-    public String readFilesAndGetDiff() throws IOException {
+    public String readFilesAndGetDiff() throws IOException, DiffException {
         try (
                 final Stream<String> streamOnLeft = FileSystemService.fromId(id).getFileContentOnLeft();
                 final Stream<String> streamOnRight = FileSystemService.fromId(id).getFileContentOnRight();) {
-            return generateDiffResult(streamOnLeft, streamOnRight);
+            final DiffRowGenerator generator = DiffRowGenerator.create().build();
+            
+            return generator.generateDiffRows(
+                        streamOnLeft.collect(Collectors.toList()),
+                        streamOnRight.collect(Collectors.toList()))
+                    .stream()
+                    .map(mapToLineWithChanges())
+                    .collect(Collectors.joining(StringUtils.LF));
         } catch (final IOException e) {
             e.printStackTrace();
-            
+
             throw e;
         }
     }
 
-    private String generateDiffResult(final Stream<String> streamOnLeft, final Stream<String> streamOnRight) {
-        final Iterator<String> iteratorOnLeft = streamOnLeft.iterator();
-        final Iterator<String> iteratorOnRight = streamOnRight.iterator();
-        final StringBuilder result = new StringBuilder();
-        
-        if (isAllLinesDeleted(iteratorOnLeft, iteratorOnRight)) {
-            iteratorOnLeft.forEachRemaining(markAsDeletedLine(result));
-        } else if (isAllLinesCreated(iteratorOnLeft, iteratorOnRight)) {
-            iteratorOnRight.forEachRemaining(markAsCreatedLine(result));
-        } else if (noLinesFound(iteratorOnLeft, iteratorOnRight)) {
-            return null;
-        } else {
-            iteratorOnLeft.forEachRemaining(markChanges(iteratorOnRight, result));
-            markRemainingLinesIfExists(iteratorOnRight, result);
-        }
-        
-        return result.toString();
-    }
-
-    private Consumer<? super String> markAsDeletedLine(final StringBuilder result) {
-        return l -> result.append(DELETED_LINE_PREFIX).append(l).append(StringUtils.LF);
-    }
-    
-    private Consumer<? super String> markAsCreatedLine(final StringBuilder result) {
-        return l -> result.append(CREATED_LINE_PREFIX).append(l).append(StringUtils.LF);
-    }
-    
-    private Consumer<? super String> markAsEqualLine(final StringBuilder result) {
-        return l -> result.append(EQUAL_LINE_PREFIX).append(l).append(StringUtils.LF);
-    }
-    
-    private Consumer<? super String> markChanges(final Iterator<String> iteratorOnRight, final StringBuilder result) {
-        return le -> {
-            if (iteratorOnRight.hasNext()) {
-                final String nextOnRight = iteratorOnRight.next();
-                
-                if (!le.equals(nextOnRight)) {
-                    result.append(DELETED_LINE_PREFIX).append(le).append(StringUtils.LF);
-                    result.append(CREATED_LINE_PREFIX).append(nextOnRight).append(StringUtils.LF);
-                } else {
-                    result.append(EQUAL_LINE_PREFIX).append(nextOnRight).append(StringUtils.LF);
-                }
+    private Function<? super DiffRow, ? extends String> mapToLineWithChanges() {
+        return r -> {
+            if (isNotNull(r)) {
+                return getNotNullChanges(r);
+            } else if (r.getNewLine() != null) {
+                return getCreatedOnRevision(r);
             } else {
-                result.append(EQUAL_LINE_PREFIX).append(le).append(StringUtils.LF);
+                return getRemovedOnRevision(r);
             }
         };
     }
-    
-    private boolean isAllLinesDeleted(final Iterator<String> iteratorOnLeft, final Iterator<String> iteratorOnRight) {
-        return iteratorOnLeft.hasNext() && !iteratorOnRight.hasNext();
-    }
-    
-    private boolean isAllLinesCreated(final Iterator<String> iteratorOnLeft, final Iterator<String> iteratorOnRight) {
-        return !iteratorOnLeft.hasNext() && iteratorOnRight.hasNext();
+
+    private String getNotNullChanges(DiffRow r) {
+        if (isEquals(r)) {
+            return getEqualsOnRevision(r);
+        } else if(isChangedOnRevision(r)) {
+            return StringUtils.join(getRemovedOnRevision(r), getCreatedOnRevision(r));
+        } else if(isRemovedOnRevision(r)) {
+            return getRemovedOnRevision(r);
+        } else {
+            return getCreatedOnRevision(r);
+        }
     }
 
-    private boolean noLinesFound(final Iterator<String> iteratorOnLeft, final Iterator<String> iteratorOnRight) {
-        return !iteratorOnLeft.hasNext() && !iteratorOnRight.hasNext();
+    private boolean isNotNull(final DiffRow row) {
+        return row.getNewLine() != null && row.getOldLine() != null;
+    }
+
+    private boolean isEquals(final DiffRow row) {
+        return row.getOldLine().equals(row.getNewLine());
+    }
+
+    private boolean isChangedOnRevision(final DiffRow row) {
+        return !row.getNewLine().isEmpty() && !row.getOldLine().isEmpty();
+    }
+
+    private boolean isRemovedOnRevision(final DiffRow row) {
+        return row.getNewLine().isEmpty() && !row.getOldLine().isEmpty();
     }
     
-    private void markRemainingLinesIfExists(final Iterator<String> iteratorOnRight, final StringBuilder result) {
-        if (iteratorOnRight.hasNext()) {
-            iteratorOnRight.forEachRemaining(markAsEqualLine(result));
-        }
+    private String getEqualsOnRevision(DiffRow r) {
+        return StringUtils.join(EQUAL_LINE_PREFIX, r.getNewLine(), StringUtils.LF);
+    }
+
+    private String getCreatedOnRevision(DiffRow r) {
+        return StringUtils.join(CREATED_LINE_PREFIX, r.getNewLine(), StringUtils.LF);
+    }
+
+    private String getRemovedOnRevision(DiffRow r) {
+        return StringUtils.join(DELETED_LINE_PREFIX, r.getOldLine(), StringUtils.LF);
     }
 }
